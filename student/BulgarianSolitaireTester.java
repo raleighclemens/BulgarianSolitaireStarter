a1ce0180c7ccc7583e5578a4dba6d8045c512940
raleighclemens <54150990+raleighclemens@users.noreply.github.com>
2020-03-24 17:54:05 -0500

    finished

--- student/BulgarianSolitaireTester.java
+++ student/BulgarianSolitaireTester.java
@@ -1,10 +1,13 @@
+import java.util.*;
 /**
    Tests the game of Bulgarian Solitaire.
+   Raleigh Clemens
 */
 public class BulgarianSolitaireTester
 {
    public static void main(String[] args)
    {
+      /*
       Piles deck = new Piles(new int[]{ 20, 5, 1, 9, 10 });
       deck.playRound();
       System.out.println(deck);
@@ -23,6 +26,69 @@ public class BulgarianSolitaireTester
       System.out.println("Expected: [15, 4, 5, 1, 2, 4, 6, 8]");
       deck.playRound();
       System.out.println(deck);
-      System.out.println("Expected: [14, 3, 4, 1, 3, 5, 7, 8]");      
+      System.out.println("Expected: [14, 3, 4, 1, 3, 5, 7, 8]"); 
+      */
+     
+      Random gen = new Random();
+      
+      //generates a random num of piles
+      int numPiles = gen.nextInt(9) + 1;
+      
+      //generates the starting hand array with the random number of piles
+      int[] startingHand = new int[numPiles];
+      
+      int sum = 0;
+      
+      //fills in each number of cards in the starting hand (except the last element)
+      for(int i=0; i<numPiles-1; i++){
+          //generates a random number within range for each element in the starting
+          //hand, depending on the number of piles in the starting hand
+          int rand = gen.nextInt(45/numPiles) + 1;
+          //this underapproximates how many cards are allowed to go in each element
+          //of the starting hand, so we will never have a negative number
+          //for the last card in the starting hand
+          
+          startingHand[i] = rand;
+          
+          sum += rand; //counts the total number of cards so we can fill in
+          //the last element, having the sum of all cards in the starting hand
+          //equal 45
+        }
+      startingHand[numPiles-1] = 45 - sum; //makes sure the last element always
+      //makes the total cards add up to 45; since the elements above are
+      //underapproximated due to integer division, this last element will never
+      //be negative, which is what we want
+      
+      Piles deck = new Piles(startingHand);
+      
+      System.out.println("STARTING: " + deck);
+      
+      //just a fun counter to see how long each arrangement takes to win
+      int moves =0;
+      
+      while(true){ //keeps running deck.playRound unless loop is broken
+          if(deck.getSize() == 9){ //makes sure we dont get an index out of bounds
+              //when we test the number of cards in each element of the deck
+              
+              int truths = 0;
+              for(int i=0; i<9; i++){
+                  if(deck.getElement(i) == i+1){ //the 0th element in deck=1 & so on
+                      //this is how to win Bulgarian solitare
+                      
+                      truths++; 
+                    }
+                }
+              if(truths == 9){ //if all nine elements in the deck are in the
+                  //winning configuration, break the loop. we've finished
+                  break;
+              }
+            }
+          deck.playRound(); //otherwise, keep playing rounds
+          //System.out.println(deck);
+          moves++;
+        }
+        
+      System.out.println("FINISHED: " + deck);
+      System.out.println("This took " + moves + " moves.");
    }
 }
--- student/Piles.java
+++ student/Piles.java
@@ -3,6 +3,7 @@ import java.util.Random;
 /**
  * A class that plays Bulgarian Solitare. 
  * Version 1: Implement the playRound and toString methods.
+ * Raleigh Clemens
  */
 public class Piles
 {
@@ -18,14 +19,20 @@ public class Piles
       for (int s : pileSizes)
          piles.add(s);
    }
-
+   
    /**
     * Return the string representation of the piles.
     * @return the string representation of the piles.
     */
    public String toString()
    {
-      . . .
+      String data = "[";
+      for(int i=0; i<piles.size()-1; i++){
+          data += piles.get(i) + ", ";
+        }
+      data += piles.get(piles.size()-1);
+      data += "]";
+      return data;
    }
 
    /**
@@ -33,6 +40,32 @@ public class Piles
    */
    public void playRound()
    {
-      . . .
+       int nextPile = 0;
+       for(int i=0; i<piles.size(); i++){
+          piles.set(i,piles.get(i)-1); //sets each element equal to 
+          //the integer at that element minus 1
+          nextPile++; //sums the total subtracted from each element and 
+          //adds it to the end of the list
+        }
+       piles.add(nextPile);
+       
+       //removes every 0 from the arraylist
+       for(int i=0; i<piles.size(); ){ 
+           if(piles.get(i) == 0){
+               piles.remove(i); 
+            }
+           else{
+               i++;
+            }
+        }
    }
+   
+   //getters to use in the testcode
+   public int getSize(){
+       return piles.size();
+    }
+    
+   public int getElement(int index){
+       return piles.get(index);
+    }
 }
